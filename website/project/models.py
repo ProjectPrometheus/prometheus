65a3f7842399d9a48eebf27b5619688ddb5bbc74
Dirk C <dirkcummings@gmail.com>
2011-07-31 02:36:47 -0700

    fixed mysql multiple primary keys error and added fk from projectmembers to projects

--- website/project/models.py
+++ website/project/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from user.models import User
+from django.contrib.auth.models import User
 
 class Project(models.Model):
     Id = models.PositiveIntegerField(primary_key = True)
@@ -7,7 +7,7 @@ class Project(models.Model):
     summary = models.TextField()
     createDate = models.DateTimeField('date created', auto_now = True)
     completeDate = models.DateTimeField('completion date', blank = True)
-    private = BooleanField(default = True)
+    private = models.BooleanField(default = True)
     controlType = ((u'M', u'Manager'),
                    (u'B', u'Board'),
                    (u'C', u'Cloud Source'))
@@ -16,13 +16,15 @@ class Milestone(models.Model):
     Id = models.PositiveIntegerField(primary_key = True)
     title = models.CharField(max_length = 250, unique = True)
     description = models.TextField()
+    projectId = models.ForeignKey(Project)
     createDate = models.DateTimeField('date created', auto_now = True)
     due = models.DateField('due date', blank = True)
     completeDate = models.DateField('date completed', blank = True)
     
 class ProjectMember(models.Model):
-    projectId = ForeignKey(Project, primary_key = True)
-    userId = ForeignKey(User, primary_key = True)
+    projectId = models.ForeignKey(Project)
+    userId = models.ForeignKey(User)
+    unique_together = ("projectId", "userId")
     memberType = ((u'P', u'Project Manager'),
                   (u'S', u'Staff'),
                   (u'O', u'Open Contributor'),
