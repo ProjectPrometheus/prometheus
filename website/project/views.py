0537455a31045f3d6345ce65f6a62abe6aa42832
Dirk C <dirkcummings@gmail.com>
2011-08-05 18:55:13 -0700

    moved project views to tmplates/project

--- website/project/views.py
+++ website/project/views.py
@@ -1,13 +1,19 @@
 # Views for a Prometheus Project
+
+# Django Specific Imports
 from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render_to_response
+from django.views.decorators.http import require_http_methods
+
+# Project Specific Imports
 from project.models import Project, ProjectMember
-from task.models import Task
-from django.http import HttpResponse
+# from task.models import Task
 
 # The landing page for all projects home page.
 # Shows a list of the top project / random projects
 def index(request):
-    return HttpResponse()
+    return render_to_response('project/index.html', {})
 
 # The "Home Page" or "Dashboard" of an individual project
 require_http_methods(["GET"])
@@ -32,10 +38,10 @@ def milestones(request, project_id):
     p_miles = get_list_or_404(Milestone, projectId = project_id)
     return render_to_response('project/milstones.html', {'miles': p_miles})
 
-require_http_methods(["GET"])
-def tasks(request, project_id):
-    p_tasks = get_list_or_404(Task, projectId = project_id)
-    return render_to_response('project/tasks.html', {'tasks': p_tasks})
+# require_http_methods(["GET"])
+# def tasks(request, project_id):
+#    p_tasks = get_list_or_404(Task, projectId = project_id)
+#    return render_to_response('project/tasks.html', {'tasks': p_tasks})
 
 require_http_methods(["GET"])
 def add(request):
